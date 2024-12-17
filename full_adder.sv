6679ce4e536d2695ca21125feb22cebb157efaaf
Sidhartha Saha <157287564+sidhartha-saha@users.noreply.github.com>
2024-12-17 23:15:01 +0600

    Update full_adder.sv

--- full_adder.sv
+++ full_adder.sv
@@ -8,3 +8,29 @@ assign sum = a^b^Cin;
 assign carry = ((a^b)&Cin)|(a&b);
 
 endmodule
+
+module fa_tb_top();
+	reg [1:0]a;
+	reg [1:0]b;
+    reg [1:0]Cin;
+	wire [2:0]sum;
+    wire carry;
+
+
+ full_adder dut( .a(a),
+				         .b(b),
+				         .Cin(Cin),
+                 .sum(sum),
+                 .carry(carry)
+				        	);
+
+	initial begin
+   $monitor($time,"ns || A = %b || B = %b || Cin = %b || Sum = %b || Carry = %b",a,b,Cin,sum,carry);
+
+	a = 2'b00 ; b = 2'b11 ; Cin = 2'b10; #5;
+	a = 2'b01 ; b = 2'b11 ; Cin = 2'b10;  #5;
+	a = 2'b01 ; b = 2'b10 ; Cin = 2'b10; #5;
+	$finish;
+
+	end
+endmodule
